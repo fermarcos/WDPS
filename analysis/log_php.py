58f724d7bfa665e00342f95354fae74d1ffc75d5
dianaArrieta <diana.arrieta@outlook.es>
2017-10-29 14:07:02 -0600

    Add files via upload
    Agregue los contadores para los errores.

--- analysis/log_php.py
+++ analysis/log_php.py
@@ -25,6 +25,11 @@ r_php = prueba()
 def analyzePhpLogs(logs):
 
 	for l in logs:
+		error_fatal = 0
+		error_parse = 0
+		error_warning = 0
+		error_notice = 0
+		error_deprecated = 0
 
 		file_reporte = open ("reporte-"+l+".txt","w")
 
@@ -38,14 +43,23 @@ def analyzePhpLogs(logs):
 					err = re.search('PHP ([a-zA-Z]+)( [a-zA-Z]+)?', line)
 					if err is not None:
 						r_php.c_error = err.group(0)
-						r_php.fecha = ' '.join(line.split()[0:5]).strip("]").strip("[")
-						r_php.fecha = re.sub(r'\.[0-9]+ ',' ',r_php.fecha) 
-						r_php.desc =re.search(r'(PHP ([a-zA-Z]+)( [a-zA-Z]+)?:  )(.+)( in .+)',line)
-						r_php.archivo = re.search(r'( in )(.+)',line)
-						r_php.archivo = r_php.archivo.group(2)
+						if re.search("Fatal", r_php.c_error):
+							error_fatal += 1
+						if re.search("Parse", r_php.c_error):
+                					error_parse += 1
+						if re.search("Warning", r_php.c_error):
+                					error_warning += 1
+						if re.search("Notice", r_php.c_error):
+               						error_notice += 1
+						if re.search("Deprecated", r_php.c_error):
+                					error_deprecated += 1
+						r_php.fecha = re.sub(r'\.[0-9]+ ',' ',' '.join(line.split()[0:5]).strip("]").strip("[")) 
+						r_php.desc =re.search(r'(PHP ([a-zA-Z]+)( [a-zA-Z]+)?:  )(.+)( in .+)',line).group(4)
+						r_php.archivo = re.search(r'( in )(.+)',line).group(2)
 
-						file_reporte.write(r_php.c_error + "\t"  + r_php.fecha + "   " + r_php.desc.group(4) + "\t" + r_php.archivo + "\n")
+						file_reporte.write(r_php.c_error + "\t"  + r_php.fecha + "   " + r_php.desc + "\t" + r_php.archivo + "\n")
 						#print r_php.c_error + "\t"  + r_php.fecha + "\t" + r_php.desc.group(4) + "\t" + r_php.archivo 
+		print "\nError faltal: " + str(error_fatal) + "\nWarning: "  + str(error_warning) + "\nNotice: " + str(error_notice)  + "\nParse error: "+ str(error_parse) +"\nFunction deprecated: " + str(error_deprecated)
 
 
 		file_reporte.close
